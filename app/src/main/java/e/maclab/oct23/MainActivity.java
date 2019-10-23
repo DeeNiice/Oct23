158627abb808cb7dc05b584eef8dda4d5a79be81
DeeNiice <2015082631@ust-ics.mygbiz.com>
2019-10-23 20:09:03 +0800

    rtrter

--- app/src/main/java/e/maclab/oct23/MainActivity.java
+++ app/src/main/java/e/maclab/oct23/MainActivity.java
@@ -1,7 +1,9 @@
 package e.maclab.oct23;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //R.drawable.accountancy, R.drawable.archi, R.drawable.cfad, R.drawable.civil_law, R.drawable.commerce, R.drawable.educ, R.drawable.engg, R.drawable.iics
     };
 
+    String[] collegeInfo = {"Accountancy message.....", "Arki message.....", "cfad message.....", "civil law message.....", "Commerce message.....", "Educ message....."  };
     ArrayList<College> collegeList = new ArrayList;
 
     @Override
@@ -34,7 +37,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             collegeList.add(new College(cLogo[i], names[i], saint[i]));
         }
         list = findViewbyId(R.id.lvColleges);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, );
+       // ArrayAdapter adapter = new ArrayAdapter<String>(this, );
+
 
     }
 
@@ -42,7 +46,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id){
 
-        Toast.makeText(this, names[i], Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, names[i], Toast.LENGTH_LONG).show();
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(collegeList.get(i).getName());
+        dialog.setIcon(collegeList.get(i).getLogo);
+        dialog.setMessage(collegeInfo[i]);
+        dialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.create().show();
 
     }
+
+
 }
